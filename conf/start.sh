76a5da821fa341c583fd5d58349ddccaf2ba0a0c
SJ Chou <public.sj@gmail.com>
2017-07-14 13:50:46 +0800

    first docker file and resource commit

---
+++ conf/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $RAM != "" ]; then
+  echo "Setup RAM: $RAM static process for php-fpm"
+  cp /tmp/www-${RAM}.conf /etc/php5/fpm/pool.d/www.conf
+else
+  echo "Setup RAM: $RAM dynamic process for php-fpm"
+fi
+
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
